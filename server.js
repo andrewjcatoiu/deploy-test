64c1fa80e228374a7be1c62b11a81d00c202ece2
andrewjcatoiu <112202202+andrewjcatoiu@users.noreply.github.com>
2023-11-16 15:14:07 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("works home")
+})
+
+app.get("/new", (req, res) => {
+    res.send("also works")
+})
+
+
+
+
+app.listen(process.env.PORT || 5000)
+module.exports = app
