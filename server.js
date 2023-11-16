6106db7d89331115cd62e253c48e0160a5671577
ajcatoiu <ajcatoiu@dons.usfca.edu>
2023-11-16 15:24:04 -0800

    sec

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("works home")
+    res.send("works homje")
 })
 
 app.get("/new", (req, res) => {
